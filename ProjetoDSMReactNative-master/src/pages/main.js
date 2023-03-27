6c058e924495ee1856917271f21d611f3b9c590a
gabriela <cardoso.gabicardoso@gmail.com>
2023-03-26 21:06:01 -0300

    aula sabado

--- ProjetoDSMReactNative-master/src/pages/main.js
+++ ProjetoDSMReactNative-master/src/pages/main.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { Container, Form, Input, SubmitButton } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default class Main extends Component {
-  render() {
+export default function Main() {
     return (
-      <View>
-        <Text> Página Main </Text>
-      </View>
+      <Container>
+        <Form>
+          <Input autoCorrect={false}
+          autoCapitalize='none'
+          placeholder= 'Adicionar usuário'
+           />
+           <SubmitButton>
+            <Icon name='add' size={20} color='#fff' />
+           </SubmitButton>
+        </Form>
+      </Container>
     );
   }
-}
---
+++ ProjetoDSMReactNative-master/src/pages/styles.js
@@ -0,0 +1,23 @@
+import style from  'styled-components/native'; 
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+flex: 1;
+padding: 30px;
+`;
+
+export const Form = styled.View`
+flex-direction: row;
+padding-botton: 20px;
+border-bottom-width: 1px;
+border-color: #eee;
+`;
+
+export const Input = styled.TextInput`
+flex: 1;
+height: 40;
+background: #
+`;
+
+export const SubmitButton = styled(RectButton)`
+`;
